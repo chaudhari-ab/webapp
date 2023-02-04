705d313fcfde1950d544511e051f353bddf10715
Abhishek <chaudhari.ab@northeastern.edu>
2023-02-04 14:28:01 -0500

    Demo 2

--- src/test/java/com/product/crud/CrudApplicationTests.java
+++ src/test/java/com/product/crud/CrudApplicationTests.java
@@ -39,7 +39,7 @@ class CrudApplicationTests {
         user.setAccount_created(LocalDateTime.now());
         user.setAccount_updated(LocalDateTime.now());
         user.setUsername("edsdsd");
-        String username = "Abhishek";
+        String username = ""
         when(repository.findByUsername(username)).thenReturn(user);
         assertEquals(user.getUsername(), service.loadUserByUsername(username).getUsername());
     }
